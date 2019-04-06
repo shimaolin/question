5f8a6ee536f3ad3a0a96085f4ec026cfb4b4392c
curitis-tang@gmail.com <curitis.tang@gmail.com>
2019-04-06 20:28:34 +0800

    modify index

--- src/main/java/org/curitis/controller/IndexController.java
+++ src/main/java/org/curitis/controller/IndexController.java
@@ -1,14 +1,19 @@
 package org.curitis.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/hello")
-public class HelloController {
+@Controller
+public class IndexController {
 
     @RequestMapping("/world")
     public String world(){
         return "Hello World!";
     }
+
+    @RequestMapping("/")
+    public String index(){
+        return "redirect:/type/";
+    }
+
 }
